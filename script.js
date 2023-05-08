21baa971cfb659a9eaad9267f3848ffb10d034e8
Alvaro Padilla <alvaro.padilla2004@gmail.com>
2023-05-08 12:00:05 -0400

    se incluyen comentarios

--- script.js
+++ script.js
@@ -10,7 +10,6 @@ encryptButton.addEventListener("click", ()=>{
     var textencrypt = textinput.value;
     resultpanel.classList.add('active-rightSection__resultpanel');
     textResult.classList.add('active-rightSection__resultBar');
-    console.log(textencrypt);
     textencrypt = textencrypt.replace(/e/g,'enter');
     textencrypt = textencrypt.replace(/i/g,'imes');
     textencrypt = textencrypt.replace(/a/g,'ai');
@@ -23,7 +22,6 @@ decryptButton.addEventListener("click", ()=>{
     var textdecrypt = textinput.value;
     resultpanel.classList.add('active-rightSection__resultpanel');
     textResult.classList.add('active-rightSection__resultBar');
-    console.log(textdecrypt);
     textdecrypt = textdecrypt.replace(/enter/g,'e');
     textdecrypt = textdecrypt.replace(/imes/g,'i');
     textdecrypt = textdecrypt.replace(/ai/g,'a');
